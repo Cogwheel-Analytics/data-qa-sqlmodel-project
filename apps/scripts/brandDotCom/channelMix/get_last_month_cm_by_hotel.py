b65f83bbbbe7f99823ffdf0ea086c6701651bb58
mehedicwa <mehedi@cogwheelanalytics.com>
2025-04-16 23:21:01 +0600

    add query for only active hotels

--- apps/scripts/brandDotCom/channelMix/get_last_month_cm_by_hotel.py
+++ apps/scripts/brandDotCom/channelMix/get_last_month_cm_by_hotel.py
@@ -3,15 +3,17 @@ from apps.database import get_session
 
 hotel_id = "b80acf46-4456-4972-a575-1709f4ee7d1a"
 
-# SQL query to fetch channel_mix data for the specific hotel and last month
+# SQL query to fetch channel_mix data for the specific active hotel and last x months
 query = text(
     """
-    SELECT *
-    FROM public.channel_mix
-    WHERE date >= DATE_TRUNC('month', CURRENT_DATE) - INTERVAL '3 month'
-      AND date < DATE_TRUNC('month', CURRENT_DATE)
-      AND hotel_id = :hotel_id
-    ORDER BY date;
+    SELECT cm.*
+    FROM public.channel_mix cm
+    JOIN public.hotel h ON cm.hotel_id = h.id
+    WHERE cm.date >= DATE_TRUNC('month', CURRENT_DATE) - INTERVAL '1 month'
+      AND cm.date < DATE_TRUNC('month', CURRENT_DATE)
+      AND cm.hotel_id = :hotel_id
+      AND h.is_active = true
+    ORDER BY cm.date;
     """
 )
 
@@ -26,9 +28,9 @@ def get_last_month_channel_mix_by_hotel(hotel_id: str):
 channel_mix_data = get_last_month_channel_mix_by_hotel(hotel_id)
 
 if channel_mix_data:
-    print("Channel Mix data for last month (single hotel):\n")
+    print("Channel Mix data for the last x months (active hotel):\n")
     for row in channel_mix_data:
         print(row)
     print(f"\nTotal records: {len(channel_mix_data)}")
 else:
-    print("No data found for this hotel in last month.")
+    print("No data found for this hotel in the last x months or the hotel is inactive.")
