04fbab87f54699c29fc3575852cabb6353e89e6d
mehedicwa <mehedi@cogwheelanalytics.com>
2025-04-22 21:54:32 +0600

    get all missing paid media (sponsored listing) hotel for last x months

--- apps/scripts/paidMedia/sponsoredListing/missingData/last_x_month_missing_sponlist_data.py
+++ apps/scripts/paidMedia/sponsoredListing/missingData/last_x_month_missing_sponlist_data.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 from sqlmodel import text
 from apps.database import get_session
 
-# SQL to find missing or empty PaidMedia (Meta Search) data for the last 6 months
+# SQL to find missing or empty PaidMedia (Sponsored Listing) data for the last 6 months
 query = text(
     """
     WITH
@@ -30,37 +30,37 @@ query = text(
             active_hotels h
             CROSS JOIN months m
     ),
-    meta_search_paid_media AS (
+    sponsored_listing_paid_media AS (
         SELECT
             hotel_id,
             DATE_TRUNC ('month', date) AS month_start,
-            COUNT(*) AS meta_entries
+            COUNT(*) AS sponsored_listing_entries
         FROM
             public.paid_media pm
             JOIN public.media_channel mc ON pm.media_id = mc.id
         WHERE
-            mc.name = 'METASEARCH'
+            mc.name = 'SPONSORED LISTING'
             AND pm.date >= DATE_TRUNC ('month', CURRENT_DATE) - INTERVAL '6 month'
             AND pm.date < DATE_TRUNC ('month', CURRENT_DATE)
         GROUP BY
             hotel_id,
             DATE_TRUNC ('month', date)
     ),
-    missing_meta_data AS (
+    missing_sponsored_listing_data AS (
         SELECT
             hmc.hotel_code,
             TO_CHAR (hmc.month_start, 'YYYY-MM') AS month
         FROM
             hotel_month_combinations hmc
-            LEFT JOIN meta_search_paid_media ms ON hmc.hotel_id = ms.hotel_id
+            LEFT JOIN sponsored_listing_paid_media ms ON hmc.hotel_id = ms.hotel_id
             AND hmc.month_start = ms.month_start
         WHERE
-            ms.meta_entries IS NULL
+            ms.sponsored_listing_entries IS NULL
     )
 SELECT
     *
 FROM
-    missing_meta_data
+    missing_sponsored_listing_data
 ORDER BY
     hotel_code,
     month;
@@ -81,10 +81,9 @@ hotel_months = defaultdict(list)
 for row in missing_rows:
     hotel_months[row.hotel_code].append(row.month)
 
-
 if hotel_months:
-    print("Hotels with Missing Meta Search (Paid Media) Data (Last 6 Months):\n")
+    print("Hotels with Missing Sponsored Listing (Paid Media) Data (Last 6 Months):\n")
     for hotel_code, months in hotel_months.items():
         print(f"Hotel Code: {hotel_code}, Missing Months: {months}")
 else:
-    print("All active hotels have meta search data for the past 6 months.")
+    print("All active hotels have Sponsored Listing data for the past 6 months.")
